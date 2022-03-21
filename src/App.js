ecb84453b97de40352d7d711d76833721fc1e8ca
prabhanshjn <prabhanshjain@gmail.com>
2022-03-21 09:25:54 +0530

    FInished Todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+  const [todos,setTodos] = React.useState(() => JSON.parse(localStorage.getItem("todos")) || [])
+  const [todo, setTodo] = React.useState("")
+  const [editTodoId, setEditTodoId] = React.useState(null)
+  const [editTodoText,setEditTodoText] = React.useState("")
+
+  React.useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+
+
+    function handleSubmit(e){
+      e.preventDefault()
+
+      if(todo === ""){
+        alert("Empty Todo is Not Allowed")
+
+      }else{
+        const newTodo = {
+          id: new Date().getTime(),
+          text: todo,
+          completed: false,
+          dateModified: new Date().toDateString()
+        }
+
+        setTodos([...todos].concat([newTodo]))
+
+      }
+
+
+        setTodo("")
+    }
+    function deleteTodo(id){
+      const updatedTodo = [...todos].filter((todo) => id !== todo.id)
+
+        setTodos(updatedTodo)
+    }
+    function editTodo(id){
+
+      const updatedTodo = [...todos].map((todo) => {
+        if(todo.id === id){
+          todo.text = editTodoText;
+        }
+        return todo
+      })
+
+      setTodos(updatedTodo)
+      setEditTodoText("")
+      setEditTodoId(null)
+    }
+
+    function checkTodo(id){
+    const updatedTodos = [...todos].map((todo) => {
+
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+
+      setTodos(updatedTodos)
+  }
+
+  function copyTodo(id){
+    [...todos].map((todo) => {
+
+      if(todo.id === id){
+        const newTodo = {
+          id: new Date().getTime(),
+          text: todo.text,
+          completed: false,
+          dateModified: new Date().toDateString()
+        }
+        setTodos([...todos].concat([newTodo]))
+      }
+
+    })
+  }
+
+  return(
+      <div className={"todo"}>
+
+        <form onSubmit={handleSubmit}>
+          <input type={"text"} value={todo} onChange={(e) => setTodo(e.target.value)}/>
+            <button className={"add-todo"} type={"submit"}>Add Todo</button>
+        </form>
+
+
+        <ol>
+          {todos.map((todo) =>
+
+
+              <div key={todo.id}>
+
+                <li>
+
+            {todo.id === editTodoId?
+                (<input type={"text"} value={editTodoText} onChange={(e) => setEditTodoText(e.target.value)}/>) :
+
+                (<div className={todo.completed? ("completedTodo"):(null)}>{todo.text}</div>)
+            }
+            <span> {todo.dateModified}</span>
+
+              <button className={"margin-buttons"} onClick={() => deleteTodo(todo.id)}>Delete</button>
+
+            {todo.id === editTodoId? (<button onClick={() => editTodo(todo.id)}>Submit Edit</button>) : (<button onClick={() => setEditTodoId(todo.id)}>Edit Todo</button>)}
+              <input onClick={() => checkTodo(todo.id)} type={"checkbox"}/>
+            <span>Completed?</span>
+
+            <button className={"margin-buttons"} onClick={() => copyTodo(todo.id)}>Copy Todo</button>
+
+              </li>
+
+
+          </div>)}
+        </ol>
+
+      </div>
+  )
 }
-
-export default App;
