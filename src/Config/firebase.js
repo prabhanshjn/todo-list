8ccd536913f1a799889265358ddccf1d0bcd096a
prabhanshjn <prabhanshjain@gmail.com>
2022-03-25 00:08:01 +0530

    Changed to typescript

---
+++ src/Config/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBGsvO0xhVeTftbnoApw0qFqvlo__AGhJE",
+    authDomain: "todo-list-8eebb.firebaseapp.com",
+    projectId: "todo-list-8eebb",
+    storageBucket: "todo-list-8eebb.appspot.com",
+    messagingSenderId: "323757872546",
+    appId: "1:323757872546:web:9c406b66d5f83714bd4efb",
+    measurementId: "G-R4TQYGL48P"
+};
+
+
